3826b85b3967bad1f17d7c0b7ee41900367f9aa8
DimoSlavov <dimo_slavov87@abv.bg>
2024-01-10 10:03:02 +0200

    task

---
+++ 02.Csharp-Advanced/09. Functional Programming - Lab/03. Count Uppercase Words/CountUppercaseWords.cs
@@ -0,0 +1,21 @@
+﻿namespace CountUppercaseWords;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class CountUppercaseWords
+{
+    private static void Main()
+    {
+        Func<string, bool> isUperIndexZero = word => char.IsUpper(word[0]);
+
+        string[] words = Console.ReadLine()
+            .Split(" " , StringSplitOptions.RemoveEmptyEntries)
+            .Where(isUperIndexZero)
+            .ToArray();
+
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
